80cc845fa1b2fe299372e63bd8c8119d59b161a4
Archit's 25th Birthday Quest <152522191+Archits25@users.noreply.github.com>
2023-12-03 23:56:19 +0530

    Update day_4_challenge.py

--- day_4/day_4_challenge.py
+++ day_4/day_4_challenge.py
@@ -2,7 +2,7 @@ def main():
     print("ONLYYYYYY 9 to goooooooooo <3)! 🎉🎂")
 
     github_repo_url = "https://github.com/Archits25/Archits25"
-    image_path = "/path/to/your/image.jpg"
+    image_path = "/day_4/Birthday Teaser.mp4"
     image_link = f"{github_repo_url}/blob/main{image_path}"
     
     print("To see your birthday image, click on the link below:")
