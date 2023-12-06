d5b899cc14d821d63644aec07e75d6a2f12e5fc6
Archit's 25th Birthday Quest <152522191+Archits25@users.noreply.github.com>
2023-12-06 22:11:21 +0530

    Create day_7_challenge_2.py

---
+++ day_7/day_7_challenge_2.py
@@ -0,0 +1,11 @@
+def decode_code(String message):
+  #your code here
+
+def main():
+  print("Edit the file and after editing run it for the following coded message - 'Wkh dwayl rq ehvw errslqj txlfnv lv - DUFLWLQ PDJHVWKBWX'")
+  print("Note: Open day_7_challenge_3 - only if your struck somewhere otherwise head directly to day_7_challenge_4")
+
+if __name__ == "__main__":
+    main()
+
+  
