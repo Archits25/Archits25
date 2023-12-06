a434f9a7f155c6364867d4462be615bd9a1a473c
Archit's 25th Birthday Quest <152522191+Archits25@users.noreply.github.com>
2023-12-06 22:04:26 +0530

    Update day_7_challenge_1.py

--- day_7/day_7_challenge_1.py
+++ day_7/day_7_challenge_1.py
@@ -1,10 +1,14 @@
 def main():
-    print("ONLYYYYYY 5 to goooooooooo <3)! 🎉🎂")
+    print("ONLYYYYYY 8 to goooooooooo <3)! 🎉🎂")
     coded_message = "Wkh dwayl rq ehvw errslqj txlfnv lv - DUFLWLQ PDJHVWKBWX"
-    start_choice = input("Are you ready?? Y:N")
-    print("Whether you are ready or not but we are ready with the Day 7 quest of the Archits25 quest")
-    print("So the challenge for today is - Today I met The Universe creator and they gave me this phrase '{coded_message}' - They said if I could decipher this message I would get to know athe greatest truth of all times...Can you help me in deciphering this code?")
-    print("In order to help me, you need to decode the message encoded (using a Caesar cipher) in the above code. Head on to next file and start writing your code :)")
+    start_choice = input("Are you ready?? Y:N ")
+    
+    print("Whether you are ready or not, we are ready with the Day 7 quest of the Archits25 quest")
+    print(f"So the challenge for today is - Today I met The Universe creator, and they gave me this phrase '{coded_message}'")
+    print("They said if I could decipher this message, I would get to know the greatest truth of all times...")
+    print("Can you help me in deciphering this code?")
+    print("In order to help me, you need to decode the message encoded (using a Caesar cipher) in the above code.")
+    print("Head on to the next file and start writing your code :)")
 
 if __name__ == "__main__":
     main()
