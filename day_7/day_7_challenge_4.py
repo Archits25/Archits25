61cd19325bce6014667d1ce1e4ca6dd45f9390e0
Archit's 25th Birthday Quest <152522191+Archits25@users.noreply.github.com>
2023-12-06 22:16:37 +0530

    Create day_7_challenge_4.py

---
+++ day_7/day_7_challenge_4.py
@@ -0,0 +1,4 @@
+def main():
+    print("Hi my baby, here's your present - download the resource here - https://github.com/Archits25/Archits25/blob/main/day_7/b9b00c062cb9846b1e9d97b0a8b506d40348f76d.pdf")
+if __name__ == "__main__":
+    main()
