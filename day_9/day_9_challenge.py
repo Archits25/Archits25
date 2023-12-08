e53b1270bb002cd16ca345ed629e938c2d9157b8
Archit's 25th Birthday Quest <152522191+Archits25@users.noreply.github.com>
2023-12-08 16:24:37 +0530

    Create day_9_challenge.py

---
+++ day_9/day_9_challenge.py
@@ -0,0 +1,12 @@
+def main():
+    print("yayayyayayya only 4 days left 🎉🎂")
+    print("To know your Day_8 gift you have to answer the following riddles - ")
+    
+    ans_1 = (input("Are you ready??"))
+    
+    print("This is an easy one but a memory game - without seeing your earlier answer - you just have to decrease the number by one and follow the same link")
+
+    print("Put your answers in the dashes in this link - https://github.com/Archits25/Archits25/blob/main/day_8/----.mp4")
+
+if __name__ == "__main__":
+    main()
