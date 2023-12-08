881ab1cafce640cc49507c9c3840be91c2920125
Archit's 25th Birthday Quest <152522191+Archits25@users.noreply.github.com>
2023-12-08 16:17:25 +0530

    Create day_8_challenge_1.py

---
+++ day_8/day_8_challenge_1.py
@@ -0,0 +1,16 @@
+def main():
+    print("yayayyayayya your birthday is just round the corner 🎉🎂")
+    print("To know your Day_8 gift you have to answer the following riddles - ")
+    
+    ans_1 = int(input("I am a number less than ten, Greater than three, but not eight. Add me to a week, not on its end, What am I? Unravel my fate."))
+    
+    if ans_1 == 5:
+        print("Great...moving on to the next quest")
+
+    ans_2 = input("I mark the passage of light, From dawn to the silent night. Without me, time seems astray, What am I? Solve and find your way.")
+    
+    if ans_2.lower() == "day":
+        print("Awesomeeee, let's go towards finding the surprise for today - to find it, put your answers in this link - https://medium.com/@sugaemails/happy-25th-cb25c0a273fb")
+
+if __name__ == "__main__":
+    main()
