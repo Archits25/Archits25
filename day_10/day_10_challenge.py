24bdc69812d00b7b1361488ca4f7f07d2f17aeae
Archit's 25th Birthday Quest <152522191+Archits25@users.noreply.github.com>
2023-12-09 18:32:05 +0530

    Create day_10_challenge.py

---
+++ day_10/day_10_challenge.py
@@ -0,0 +1,11 @@
+def main():
+    print("yayayyayayya only 3 days to goooooo!! 🎉🎂")
+
+    # Link to the Medium article
+    logo_url = "https://medium.com/@sugaemails/happy-25th-cb25c0a273fb"
+    
+    print("To unwrap your birthday present, click on the link below:")
+    print(logo_url)
+
+if __name__ == "__main__":
+    main()
