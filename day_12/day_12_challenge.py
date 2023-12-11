b61254645bb6da7dfda24d6f08ad6d9f739f841e
Archit's 25th Birthday Quest <152522191+Archits25@users.noreply.github.com>
2023-12-11 20:22:56 +0530

    Create day_12_challenge.py

---
+++ day_12/day_12_challenge.py
@@ -0,0 +1,15 @@
+def main():
+    print("🚀 Welcome to Day 12 of the Archit's 25th Birthday Quest! 🚀")
+    print("Your quest today involves exploring a realm of possibilities!")
+
+    explore_website = input("Sooooo are you ready for your today's gift? (Enter 'yes' or 'no'): ").lower()
+
+    if explore_website == "yes":
+        print("🎉 Fantastic! Your curiosity knows no bounds. Let's unravel the next layer.")
+        print("🔗 For the next step, follow the link to discover your Day 12 gift:")
+        print("   https://github.com/Archits25/Archits25/blob/main/day_12/secret_gift.txt")
+    else:
+        print("⏳ Take your time exploring, and when you're ready, return here to unveil the next surprise.")
+
+if __name__ == "__main__":
+    main()
